08acc7fbba056c0e77fd75da84ca3d90b406773f
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-12-01 12:32:37 +0100

    Completed Day 1 2019

---
+++ 2019/day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using AOC.Resources;
+using System.Linq;
+
+namespace day01
+{
+    /**
+     * https://adventofcode.com/2019/day/1
+     */
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            int[] moduleMass = General.ReadIntegerInput(Days.Day01);
+
+            int[] fuelRequirements = CalculateFuelRequirements(moduleMass);
+            int result = fuelRequirements.Sum();
+
+            General.PrintResult("The sum of the fuel requirements is", result);
+        }
+
+        private static int[] CalculateFuelRequirements(int[] moduleMass)
+        {
+            int[] fuelRequirements = new int[moduleMass.Length];
+            for (int i = 0; i < moduleMass.Length; i++)
+                fuelRequirements[i] = moduleMass[i] / 3 - 2;
+
+            return fuelRequirements;
+        }
+    }
+}
---
+++ 2019/day01b/Program.cs
@@ -0,0 +1,43 @@
+﻿using AOC.Resources;
+using System.Linq;
+
+namespace day01b
+{
+    /**
+     * https://adventofcode.com/2019/day/1#part2
+     */
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            int[] moduleMass = General.ReadIntegerInput(Days.Day01);
+
+            int[] fuelRequirements = CalculateFuelRequirements(moduleMass);
+            int result = fuelRequirements.Sum();
+
+            General.PrintResult("The total sum of the fuel requirements is", result);
+        }
+
+        private static int[] CalculateFuelRequirements(int[] moduleMass)
+        {
+            int[] fuelRequirements = new int[moduleMass.Length];
+            for (int i = 0; i < moduleMass.Length; i++)
+            {
+                fuelRequirements[i] = CalculateFuelRequirements(moduleMass[i]);
+            }
+
+            return fuelRequirements;
+        }
+
+        private static int CalculateFuelRequirements(int mass)
+        {
+            int fuelRequirement = mass / 3 - 2;
+            if (fuelRequirement <= 0)
+                return 0;
+
+            fuelRequirement += CalculateFuelRequirements(fuelRequirement);
+            return fuelRequirement;
+        }
+    }
+}
