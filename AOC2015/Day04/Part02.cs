69bbd572d1f2eb2fac249d2e3bdc748eeab4dc24
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-12-06 15:21:32 +0100

    Completed 2015D04

---
+++ AOC2015/Day04/Part02.cs
@@ -0,0 +1,45 @@
+﻿using AOC.Resources;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AOC2015.Day04
+{
+    /**
+     * https://adventofcode.com/2015/day/4
+     */
+
+    public static class Part02
+    {
+        public static void Exec()
+        {
+            string key = General.ReadSingleLineInput(Day.Day04);
+            int result = CalculateResult(key);
+
+            General.PrintResult(result);
+        }
+
+        private static int CalculateResult(string key)
+        {
+            using var md5 = new MD5CryptoServiceProvider();
+            md5.Initialize();
+
+            int index = 1;
+            while (true)
+            {
+                string input = key + index;
+                byte[] bInput = Encoding.UTF8.GetBytes(input);
+                byte[] hash = md5.ComputeHash(bInput);
+
+                // Detect an hash starting with three blank bytes.
+                if (hash[0] == 0 &&
+                    hash[1] == 0 &&
+                    hash[2] == 0)
+                {
+                    return index;
+                }
+
+                index++;
+            }
+        }
+    }
+}
