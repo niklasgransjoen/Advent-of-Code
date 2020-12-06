70a581928ee7e9a4af2aa0dbddee932e340d85cf
Vivian Vea <vivianvea@hotmail.com>
2020-12-06 14:36:29 +0100

    Solved 2020D06

---
+++ CSharp/AOC.Y2020/Day06/Part01.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC.Y2020.Day06
+{
+    public static class Part01
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetInputLines();
+            var answersPerGroup = GetAnswersPerGroup(input);
+
+            var result = answersPerGroup.Sum(answers => answers.Count);
+            AOCUtils.PrintResult(result);
+        }
+
+        private static HashSet<char>[] GetAnswersPerGroup(string[] input)
+        {
+            var result = new List<HashSet<char>> { new() };
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    result.Add(new());
+                    continue;
+                }
+
+                foreach (var answer in line)
+                {
+                    result[^1].Add(answer);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ CSharp/AOC.Y2020/Day06/Part02.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC.Y2020.Day06
+{
+    public static class Part02
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetInputLines();
+            var answersPerGroup = GetAnswersPerGroup(input);
+
+            var result = answersPerGroup.Sum(answers => answers.Count);
+            AOCUtils.PrintResult(result);
+        }
+
+        private static HashSet<char>[] GetAnswersPerGroup(string[] input)
+        {
+            var groupedAnswers = GroupAnswers(input);
+
+            var result = new HashSet<char>[groupedAnswers.Count];
+            for (int i = 0; i < groupedAnswers.Count; i++)
+            {
+                result[i] = new();
+
+                var allAnswers = groupedAnswers[i];
+                if (allAnswers.Count == 0)
+                    continue;
+
+                var firstAnswers = allAnswers[^1];
+                foreach (var answer in firstAnswers)
+                {
+                    if (allAnswers.All(a => a.Contains(answer)))
+                        result[i].Add(answer);
+                }
+            }
+
+            return result;
+        }
+
+        private static List<List<HashSet<char>>> GroupAnswers(string[] input)
+        {
+            if (input.Length == 0)
+                return new();
+
+            var groupedAnswers = new List<List<HashSet<char>>> { new() };
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    groupedAnswers.Add(new());
+                    continue;
+                }
+
+                groupedAnswers[^1].Add(line.ToHashSet());
+            }
+
+            return groupedAnswers;
+        }
+    }
+}
