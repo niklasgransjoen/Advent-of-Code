f1654c837a3519f0e272c006c70e4e8b566069c1
Vivian Vea <vivianvea@hotmail.com>
2020-12-10 14:15:51 +0100

    Solved 2020D10

---
+++ CSharp/AOC.Y2020/Day10/Part01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC.Y2020.Day10
+{
+    public static class Part01
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetIntegerInput();
+            var differences = FindDifferences(input);
+
+            var result = differences.Ones * (differences.Threes + 1);
+            AOCUtils.PrintResult(result);
+        }
+
+        private static AdapterDifferences FindDifferences(int[] input)
+        {
+            var diffs = new Dictionary<int, int>
+            {
+                { 1, 0 },
+                { 2, 0 },
+                { 3, 0 },
+            };
+
+            int current = 0;
+            foreach (var value in input.OrderBy(val => val))
+            {
+                var diff = value - current;
+                if (diff is < 1 or > 3)
+                    throw new Exception($"A difference of '{diff}' is invalid.");
+
+                diffs[diff]++;
+
+                current = value;
+            }
+
+            return new AdapterDifferences(diffs[1], diffs[2], diffs[3]);
+        }
+
+        private sealed record AdapterDifferences(int Ones, int Twos, int Threes);
+    }
+}
---
+++ CSharp/AOC.Y2020/Day10/Part02.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+
+namespace AOC.Y2020.Day10
+{
+    public static class Part02
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetIntegerInput();
+            var diffs = CreateAdapterDiffs(input);
+            var combinations = CountCombinations(diffs);
+
+            AOCUtils.PrintResult(combinations);
+        }
+
+        private static int[] CreateAdapterDiffs(int[] input)
+        {
+            var orderedInput = input.OrderBy(val => val).ToArray();
+
+            var initialValue = 0;
+            var internalAdapterVal = orderedInput[^1] + 3;
+
+            var bigSeries = orderedInput.Append(internalAdapterVal);
+            var smallSeries = orderedInput.Prepend(initialValue);
+
+            return bigSeries.Zip(smallSeries, (big, small) => big - small).ToArray();
+        }
+
+        private static long CountCombinations(int[] diffs)
+        {
+            /*
+             * Occurrences of:
+             * - 3s are immutable.
+             * - single 1s are immutable.
+             * - double 1s give two combinations.
+             * - tripple 1s give four combinations
+             * - any extra 1 after this gives an extra of three combinations
+             */
+
+            long totalCombinations = 1;
+            for (int i = 0; i < diffs.Length; i++)
+            {
+                if (diffs[i] == 3)
+                    continue;
+
+                int onesCount = 1;
+                while (diffs[i + 1] == 1)
+                {
+                    onesCount++;
+                    i++;
+                }
+
+                checked
+                {
+                    totalCombinations *= onesCount switch
+                    {
+                        1 or 2 => onesCount,
+                        _ => 4 + 3 * (onesCount - 3),
+                    };
+                }
+            }
+
+            return totalCombinations;
+        }
+    }
+}
