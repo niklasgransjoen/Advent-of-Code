3d23ea396a44e612740422ee70227e2670b75463
Vivian Vea <vivianvea@hotmail.com>
2020-12-11 18:18:14 +0100

    Solved 2020D11

---
+++ CSharp/AOC.Y2020/Day11/Part01.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC.Y2020.Day11
+{
+    /**
+      * https://adventofcode.com/2020/day/11
+      */
+
+    public static class Part01
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetInputLines();
+            var initialLayout = ParseLayout(input);
+            SimulateUntilStable(initialLayout);
+
+            var takenSeats = initialLayout.SelectMany(cells => cells).Count(state => state == CellState.TakenSeat);
+            AOCUtils.PrintResult("The number of occupied seats are", takenSeats);
+        }
+
+        private static CellState[][] ParseLayout(string[] input)
+        {
+            if (input.Length == 0)
+                throw new Exception("Input cannot be empty.");
+
+            var height = input.Length;
+            var width = input[0].Length;
+            var layout = ArrayUtility.Create2D<CellState>(width, height);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (input[y].Length <= x)
+                        throw new Exception("Input is malformed.");
+
+                    var c = input[y][x];
+                    layout[y][x] = c switch
+                    {
+                        '.' => CellState.Floor,
+                        'L' => CellState.FreeSeat,
+                        _ => throw new Exception($"Unexpected char '{c}'."),
+                    };
+                }
+            }
+
+            return layout;
+        }
+
+        private static void SimulateUntilStable(CellState[][] current)
+        {
+            var next = ArrayUtility.Create2D<CellState>(current[0].Length, current.Length);
+            CopyJaggedTo(current, next);
+
+            while (true)
+            {
+                Simulate(current, next);
+                if (current.SelectMany(cells => cells)
+                    .SequenceEqual(next.SelectMany(cells => cells)))
+                {
+                    return;
+                }
+
+                CopyJaggedTo(next, current);
+            }
+
+            static void CopyJaggedTo(CellState[][] from, CellState[][] to)
+            {
+                for (int i = 0; i < from.Length; i++)
+                {
+                    from[i].CopyTo(to[i], index: 0);
+                }
+            }
+        }
+
+        private static void Simulate(CellState[][] current, CellState[][] next)
+        {
+            for (int y = 0; y < current.Length; y++)
+            {
+                for (int x = 0; x < current[y].Length; x++)
+                {
+                    var neighborCount = getNeighborCount(current, x, y);
+                    if (current[y][x] is CellState.FreeSeat && neighborCount == 0)
+                        next[y][x] = CellState.TakenSeat;
+                    else if (current[y][x] is CellState.TakenSeat && neighborCount >= 4)
+                        next[y][x] = CellState.FreeSeat;
+                }
+            }
+
+            static int getNeighborCount(CellState[][] layout, int posX, int posY)
+            {
+                int count = 0;
+
+                for (int y = posY - 1; y <= posY + 1; y++)
+                {
+                    if (y < 0 || y >= layout.Length)
+                        continue;
+
+                    for (int x = posX - 1; x <= posX + 1; x++)
+                    {
+                        if (x < 0 || x >= layout[y].Length)
+                            continue;
+
+                        if (x == posX && y == posY)
+                            continue;
+
+                        if (layout[y][x] == CellState.TakenSeat)
+                            count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        private enum CellState
+        {
+            Floor,
+            FreeSeat,
+            TakenSeat,
+        }
+    }
+}
---
+++ CSharp/AOC.Y2020/Day11/Part02.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC.Y2020.Day11
+{
+    /**
+      * https://adventofcode.com/2020/day/11
+      */
+
+    public static class Part02
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetInputLines();
+            var initialLayout = ParseLayout(input);
+            SimulateUntilStable(initialLayout);
+
+            var takenSeats = initialLayout.SelectMany(cells => cells).Count(state => state == CellState.TakenSeat);
+            AOCUtils.PrintResult("The number of occupied seats are", takenSeats);
+        }
+
+        private static CellState[][] ParseLayout(string[] input)
+        {
+            if (input.Length == 0)
+                throw new Exception("Input cannot be empty.");
+
+            var height = input.Length;
+            var width = input[0].Length;
+            var layout = ArrayUtility.Create2D<CellState>(width, height);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (input[y].Length <= x)
+                        throw new Exception("Input is malformed.");
+
+                    var c = input[y][x];
+                    layout[y][x] = c switch
+                    {
+                        '.' => CellState.Floor,
+                        'L' => CellState.FreeSeat,
+                        _ => throw new Exception($"Unexpected char '{c}'."),
+                    };
+                }
+            }
+
+            return layout;
+        }
+
+        private static void SimulateUntilStable(CellState[][] current)
+        {
+            var next = ArrayUtility.Create2D<CellState>(current[0].Length, current.Length);
+            CopyJaggedTo(current, next);
+
+            while (true)
+            {
+                Simulate(current, next);
+                if (current.SelectMany(cells => cells)
+                    .SequenceEqual(next.SelectMany(cells => cells)))
+                {
+                    return;
+                }
+
+                CopyJaggedTo(next, current);
+            }
+
+            static void CopyJaggedTo(CellState[][] from, CellState[][] to)
+            {
+                for (int i = 0; i < from.Length; i++)
+                {
+                    from[i].CopyTo(to[i], index: 0);
+                }
+            }
+        }
+
+        private static void Simulate(CellState[][] current, CellState[][] next)
+        {
+            for (int y = 0; y < current.Length; y++)
+            {
+                for (int x = 0; x < current[y].Length; x++)
+                {
+                    var neighborCount = getNeighborCount(current, x, y);
+                    if (current[y][x] is CellState.FreeSeat && neighborCount == 0)
+                        next[y][x] = CellState.TakenSeat;
+                    else if (current[y][x] is CellState.TakenSeat && neighborCount >= 5)
+                        next[y][x] = CellState.FreeSeat;
+                }
+            }
+
+            static int getNeighborCount(CellState[][] layout, int posX, int posY)
+            {
+                int count = 0;
+
+                for (int dirY = -1; dirY <= 1; dirY++)
+                {
+                    for (int dirX = -1; dirX <= 1; dirX++)
+                    {
+                        if (canSeeTakenSeat(layout, posX, posY, dirX, dirY))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            static bool canSeeTakenSeat(CellState[][] layout, int posX, int posY, int dirX, int dirY)
+            {
+                // Direction is 0.
+                if (dirX == 0 && dirY == 0)
+                {
+                    return false;
+                }
+
+                while (true)
+                {
+                    posX += dirX;
+                    posY += dirY;
+
+                    // Handle no neighbor.
+                    if (posY < 0 || posY >= layout.Length || posX < 0 || posX >= layout[posY].Length)
+                    {
+                        return false;
+                    }
+
+                    var state = layout[posY][posX];
+                    if (state == CellState.TakenSeat)
+                        return true;
+                    else if (state == CellState.FreeSeat)
+                        return false;
+                }
+            }
+        }
+
+        private enum CellState
+        {
+            Floor,
+            FreeSeat,
+            TakenSeat,
+        }
+    }
+}
