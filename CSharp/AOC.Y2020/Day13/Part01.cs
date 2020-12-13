e825e15f363bac5cd451ebe8f961f5a9e1ee1044
Vivian Vea <vivianvea@hotmail.com>
2020-12-13 16:48:44 +0100

    Solved 2020D13

---
+++ CSharp/AOC.Y2020/Day13/Part01.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AOC.Y2020.Day13
+{
+    /**
+      * https://adventofcode.com/2020/day/13
+      */
+
+    public static class Part01
+    {
+        private record PuzzleInput(int CurrentTime, int[] Frequency);
+
+        public static void Exec(AOCContext context)
+        {
+            var rawInput = context.GetInputLines();
+            var input = ParseInput(rawInput);
+            var (id, waitTime) = CalculateBusInfo(input);
+
+            var result = id * waitTime;
+            AOCUtils.PrintResult(result);
+        }
+
+        private static PuzzleInput ParseInput(string[] input)
+        {
+            if (input.Length < 2)
+                throw new Exception($"Input must contain at least two lines.");
+
+            var currentTime = int.Parse(input[0], CultureInfo.InvariantCulture);
+            var frequency = new List<int>();
+            foreach (var item in input[1].Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (item == "x")
+                    continue;
+
+                frequency.Add(int.Parse(item, CultureInfo.InvariantCulture));
+            }
+
+            return new PuzzleInput(currentTime, frequency.ToArray());
+        }
+
+        private static (int id, int waitTime) CalculateBusInfo(PuzzleInput input)
+        {
+            var currentID = -1;
+            var waitTime = int.MaxValue;
+
+            foreach (var item in input.Frequency)
+            {
+                var itemWait = item - input.CurrentTime % item;
+                if (itemWait < waitTime)
+                {
+                    currentID = item;
+                    waitTime = itemWait;
+                }
+            }
+
+            return (currentID, waitTime);
+        }
+    }
+}
---
+++ CSharp/AOC.Y2020/Day13/Part02.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace AOC.Y2020.Day13
+{
+    /**
+      * https://adventofcode.com/2020/day/13
+      */
+
+    public static class Part02
+    {
+        public static void Exec(AOCContext context)
+        {
+            var input = context.GetInputLines();
+            var frequency = ParseInput(input);
+            var result = CalculateResult(frequency);
+
+            AOCUtils.PrintResult(result);
+        }
+
+        private static int?[] ParseInput(string[] input)
+        {
+            if (input.Length < 2)
+                throw new Exception($"Input must contain at least two lines.");
+
+            var frequency = new List<int?>();
+            foreach (var item in input[1].Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (item == "x")
+                    frequency.Add(null);
+                else
+                    frequency.Add(int.Parse(item, CultureInfo.InvariantCulture));
+            }
+
+            return frequency.ToArray();
+        }
+
+        private static ulong CalculateResult(int?[] input)
+        {
+            checked
+            {
+                // Pair the non-null factors together with their index.
+                var factors = input.Select((val, index) => (val, index: (uint)index))
+                    .Where(f => f.val.HasValue)
+                    .Select(f => (value: (uint)f.val!, f.index))
+                    .Select(f => (f.value, index: f.index % f.value))
+                    .ToArray();
+
+                // We start with the first number.
+                var (firstVal, firstIndex) = factors[0];
+                ulong result = firstVal - firstIndex;
+
+                // The pattern repeats with a frequency equal to the product of all the factors (which are all primes).
+                ulong period = firstVal;
+
+                // For each following factor, we keep applying the period until it fulfills the requirement.
+                // We then multiply the period with that factor.
+                for (int i = 1; i < factors.Length; i++)
+                {
+                    var (value, index) = factors[i];
+                    while ((result + index) % value != 0)
+                    {
+                        result += period;
+                    }
+
+                    period *= value;
+                }
+
+                return result;
+            }
+        }
+    }
+}
