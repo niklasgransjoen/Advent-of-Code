a1e3e77a2a66ade0648cedf36e10799f899f3c50
Vivian Vea <vivianvea@hotmail.com>
2020-12-15 22:43:37 +0100

    Solved 2020D15

---
+++ CSharp/AOC.Y2020/Day15/Part01.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace AOC.Y2020.Day15
+{
+    /**
+      * https://adventofcode.com/2020/day/15
+      */
+
+    public static class Part01
+    {
+        public static void Exec(AOCContext context)
+        {
+            var rawInput = context.GetCSVInput();
+            var input = AOCUtils.StringToInt(rawInput);
+            var result = FindNthNumber(input, 2020);
+
+            AOCUtils.PrintResult(result);
+        }
+
+        private static int FindNthNumber(int[] initialPosition, int n)
+        {
+            // We assume each initial number is unique.
+            var ageMap = initialPosition.Take(initialPosition.Length - 1)
+                                        .Select((number, index) => (number, index: index + 1))
+                                        .ToDictionary(pair => pair.number, pair => pair.index);
+
+            var currentIndex = ageMap.Count + 1;
+            var lastSeenNumber = initialPosition[^1];
+
+            while (currentIndex < n)
+            {
+                int nextNumber;
+                if (ageMap.TryGetValue(lastSeenNumber, out var lastIndex))
+                    nextNumber = currentIndex - lastIndex;
+                else
+                    nextNumber = 0;
+
+                ageMap[lastSeenNumber] = currentIndex;
+                lastSeenNumber = nextNumber;
+
+                currentIndex++;
+            }
+
+            return lastSeenNumber;
+        }
+    }
+}
